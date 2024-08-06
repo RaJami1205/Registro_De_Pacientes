5e42b4bb62374aa5dbb64a6436d8052e0205305e
Raúl Alfaro <125586767+RaJami1205@users.noreply.github.com>
2024-08-05 21:50:54 -0600

    Add project files.

---
+++ Programa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Programa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
